c2403646a54b7c6994c8b744e67497a04fc2cb5e
Janith-Umeda <janith.rightmo@gmail.com>
2025-03-01 11:09:36 +0530

    update ecosystem configuration for production with clustering and environment variables

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -4,10 +4,14 @@ dotenv.config({ path: ".env" });
 module.exports = {
     apps: [{
         name: "wave-link",
-        script: "npm start",
+        script: "./node_modules/.bin/next",
+        args: "start",
+        instances: 2,
+        exec_mode: "cluster",
         env_prod: {
             APP_CONFIG: process.env.APP_CONFIG,
             GA_ID: process.env.GA_ID,
+            NODE_ENV: 'production'
         }
     }]
 }
